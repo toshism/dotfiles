24b83df259c9fac8af9558dc9c46fc1126c908ef
tosh <toshism@gmail.com>
2019-12-27 23:55:18 +0100

    fish: mostly go and kubernetes stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ set PATH /usr/local/bin $PATH
 set PATH ~/bin $PATH
 # set PATH /usr/local/go/bin $PATH
 set PATH /snap/bin/ $PATH
+set PATH ~/.local/bin/ $PATH
 # set PATH ~/.nimble/bin $PATH
 # set PATH ~/.cargo/bin $PATH
 # set PATH ~/.yarn/bin $PATH
@@ -13,72 +14,74 @@ set PATH /snap/bin/ $PATH
 
 # set -x GOPATH /home/tosh/dev/projects/go # the -x flag exports the variable
 # set PATH $PATH $GOPATH/bin
-
+set PATH $PATH /home/tosh.lyons/bin/go/bin
+set PATH $PATH /home/tosh.lyons/go/bin
 set -x EDITOR "emacsclient -t"
 set -x ALTERNATE_EDITOR ""
 
 # alias mux="tmuxinator"
 # alias cdrar="cd ~/dev/projects/uniregistrar"
 alias cdp="cd ~/dev/projects"
-# alias cdgo="cd $GOPATH/src"
+alias cdg="cd /home/tosh.lyons/go/src"
 # alias cds="cd $GOPATH/src/bitbucket.uniregistry.com/rar/swerve"
 alias cds="cd ~/dev/projects/scoutbee"
+alias kubectl="microk8s.kubectl"
 
 function fish_prompt
-  test $SSH_TTY; and printf (set_color red)(whoami)(set_color white)'@'(set_color yellow)(hostname)' '
+test $SSH_TTY; and printf (set_color red)(whoami)(set_color white)'@'(set_color yellow)(hostname)' '
 
-  test $USER = 'root'; and echo (set_color red)"#"
+test $USER = 'root'; and echo (set_color red)"#"
 
-  # Main
-  echo -n (set_color cyan)(prompt_pwd) (virtual_env_prompt) (set_color 3a4c55)'❯'(set_color 4a626d)'❯'(set_color 5b7786)'❯ '
+# Main
+echo -n (set_color cyan)(prompt_pwd) (virtual_env_prompt) (set_color 3a4c55)'❯'(set_color 4a626d)'❯'(set_color 5b7786)'❯ '
 end
 
 function virtual_env_prompt
-  if set -q VIRTUAL_ENV
-    echo -n -s (set_color blue) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-  end
+if set -q VIRTUAL_ENV
+echo -n -s (set_color blue) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+end
 end
 
 function fish_right_prompt
-  # last status
-  test $status != 0; and printf (set_color red)"⏎ "
+# last status
+test $status != 0; and printf (set_color red)"⏎ "
 
-  if git rev-parse ^ /dev/null
-    # Purple if branch detached else green
-    git branch -qv | grep "\*" | grep -q detached
-    and set_color purple --bold
-    or set_color green --bold
+if git rev-parse ^ /dev/null
+# Purple if branch detached else green
+git branch -qv | grep "\*" | grep -q detached
+and set_color purple --bold
+or set_color green --bold
 
-    # Need optimization on this block (eliminate space)
-    git name-rev --name-only HEAD
+# Need optimization on this block (eliminate space)
+git name-rev --name-only HEAD
 
-    # Merging state
-    git merge -q ^ /dev/null; or printf ':'(set_color red)'merge'
-    printf ' '
+# Merging state
+git merge -q ^ /dev/null; or printf ':'(set_color red)'merge'
+printf ' '
 
-    # Symbols
-    for i in (git branch -qv --no-color|grep \*|cut -d' ' -f4-|cut -d] -f1|tr , \n)\
-      (git status --porcelain | cut -c 1-2 | uniq)
-      switch $i
-        case "*[ahead *"
-          printf (set_color purple)⬆' '
-        case "*behind *"
-          printf (set_color purple)⬇' '
-        case "."
-          printf (set_color green)✚' '
-        case " D"
-          printf (set_color red)✖' '
-        case "*M*"
-          printf (set_color blue)✱' '
-        case "*R*"
-          printf (set_color purple)➜' '
-        case "*U*"
-          printf (set_color brown)═' '
-        case "??"
-          printf (set_color white)◼' '
-      end
-    end
-  end
+# Symbols
+for i in (git branch -qv --no-color|grep \*|cut -d' ' -f4-|cut -d] -f1|tr , \n)\
+(git status --porcelain | cut -c 1-2 | uniq)
+switch $i
+case "*[ahead *"
+  printf (set_color purple)⬆' '
+case "*behind *"
+  printf (set_color purple)⬇' '
+case "."
+  printf (set_color green)✚' '
+case " D"
+  printf (set_color red)✖' '
+case "*M*"
+  printf (set_color blue)✱' '
+case "*R*"
+  printf (set_color purple)➜' '
+case "*U*"
+  printf (set_color brown)═' '
+case "??"
+  printf (set_color white)◼' '
+end
+end
+end
 end
 
 # pyenv stuff
@@ -98,3 +101,4 @@ status --is-interactive; and . (pyenv virtualenv-init -|psub)
 # if status --is-login
 #     systemctl --user restart dropbox-zfs.service
 # end
+set fish_function_path $fish_function_path ~/dev/projects/plugin-foreign-env/functions
