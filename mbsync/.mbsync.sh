30899e850760c7252843bfb2e3c15422f2c7f669
tosh <toshism@gmail.com>
2020-04-20 22:48:20 +0200

    mbsync: update for scoutbee mail

--- mbsync/.mbsync.sh
+++ mbsync/.mbsync.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -o -u $LOGNAME i3)/environ)";
-#notify-send -t 3000 "Checking mail..."
-killall mbsync &>/dev/null
-/usr/bin/mbsync uniregistry
-#notify-send -t 3000 "$(/usr/local/bin/notmuch new)"
-/usr/local/bin/notmuch new
-/usr/bin/afew -tvn
+# eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -o -u $LOGNAME i3)/environ)";
+# #notify-send -t 3000 "Checking mail..."
+# killall mbsync &>/dev/null
+# /usr/bin/mbsync uniregistry
+# #notify-send -t 3000 "$(/usr/local/bin/notmuch new)"
+# /usr/local/bin/notmuch new
+# /usr/bin/afew -tvn
 
-/usr/bin/mbsync toshism
+# /usr/bin/mbsync toshism
+
+/usr/bin/mbsync scoutbee
+/usr/bin/notmuch new
