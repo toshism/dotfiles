2af3fd340d18f54563f4ccdbf658f6ea32d2c0ae
tosh <toshism@gmail.com>
2020-09-10 21:49:44 +0200

    qutebrowser: clean up

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -7,20 +7,24 @@ c.url.start_pages = "about:blank"
 c.input.insert_mode.auto_load = True
 c.input.insert_mode.auto_leave = False
 
-# aliases
-c.aliases['pr'] = 'open -t https://stash.uniregistry.com/projects/RAR/repos/uniregistrar/pull-requests'
-c.aliases['jira'] = 'open https://jira.uniregistry.com/secure/RapidBoard.jspa?rapidView=37&quickFilter=138'
+# c.fonts.monospace = "Ubuntu Mono"
+# c.fonts.statusbar = c.fonts.monospace
+# c.fonts.tabs = c.fonts.monospace
+# c.fonts.web.family.sans_serif = c.fonts.monospace
+# c.fonts.prompts = c.fonts.monospace
 
-c.fonts.monospace = "Ubuntu Mono"
-c.fonts.statusbar = c.fonts.monospace
-c.fonts.tabs = c.fonts.monospace
-c.fonts.web.family.sans_serif = c.fonts.monospace
-c.fonts.prompts = c.fonts.monospace
+c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
 
-c.content.headers.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
-
-c.hints.chars = 'asdfjkl;'
+c.hints.chars = "asdfjkl;"
 
 # keys
-config.bind('sb', 'spawn --userscript save-bookmark')
-config.bind('sj', 'spawn --userscript load-bookmark')
+config.bind("sb", "spawn --userscript save-bookmark")
+config.bind("sj", "spawn --userscript load-bookmark")
+
+# theme
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {"spacing": {"vertical": 6, "horizontal": 8}})
